258aeed16fc070416972287b2ed69d8781dcd446
zhilo <zhilo@surfstudio.ru>
2020-12-07 14:57:11 +0300

    create ColorChooseView.kt

--- app/src/main/java/ru/ozh/application/screen/TasksActivity.kt
+++ app/src/main/java/ru/ozh/application/screen/TasksActivity.kt
@@ -2,11 +2,22 @@ package ru.ozh.application.screen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import ru.ozh.application.R
+import ru.ozh.application.view.ColorChooseView
 
 class TasksActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tasks)
+
+        val addBtn = findViewById<Button>(R.id.add_task_btn)
+        val colorPicker = findViewById<ColorChooseView>(R.id.color_choose_view)
+
+
+        addBtn.setOnClickListener {
+            colorPicker.show()
+        }
     }
 }
--- app/src/main/java/ru/ozh/application/view/ColorChooseView.kt
+++ app/src/main/java/ru/ozh/application/view/ColorChooseView.kt
@@ -1,5 +1,6 @@
 package ru.ozh.application.view
 
+import android.animation.Animator
 import android.content.Context
 import android.util.AttributeSet
 import android.view.Gravity
@@ -7,8 +8,14 @@ import android.view.View
 import android.view.ViewAnimationUtils
 import android.view.animation.OvershootInterpolator
 import android.widget.FrameLayout
+import android.widget.ImageButton
+import android.widget.ViewAnimator
+import androidx.core.animation.doOnEnd
+import androidx.core.animation.doOnStart
 import androidx.core.view.children
+import androidx.core.view.isVisible
 import androidx.core.view.updateLayoutParams
+import ru.ozh.application.R
 import ru.ozh.application.utils.UiUtils.dp
 import java.lang.Math.toRadians
 import kotlin.math.cos
@@ -18,33 +25,22 @@ import kotlin.math.sin
 
 
 class ColorChooseView @JvmOverloads constructor(
-        context: Context,
-        val attributeSet: AttributeSet? = null,
-        defStyle: Int = 0
+    context: Context,
+    val attributeSet: AttributeSet? = null,
+    defStyle: Int = 0
 ) : FrameLayout(context, attributeSet, defStyle) {
 
-//    private val paint: Paint = Paint()
-//            .apply {
-//                isAntiAlias = true
-//                color = Color.WHITE
-//            }
+    private val closeBtn: ImageButton
+    private val ignoreTag = context.resources.getString(R.string.ignore_tag)
 
     init {
+        inflate(context, R.layout.layout_color_choice, this)
+        closeBtn = findViewById(R.id.close_btn)
 
-        setOnClickListener {
-            show()
+        closeBtn.setOnClickListener {
+            hide()
         }
     }
-
-//    override fun onDraw(canvas: Canvas) {
-//        super.onDraw(canvas)
-
-//        val cX = width / 2f
-//        val cY = height / 2f
-//        val radius = width / 2f
-//        paint.setShadowLayer(radius / 15, 0f, 0f, Color.BLACK)
-//        canvas.drawCircle(cX, cY, radius, paint)
-//    }
 
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
@@ -57,37 +53,50 @@ class ColorChooseView @JvmOverloads constructor(
     override fun onLayout(changed: Boolean, left: Int, top: Int, right: Int, bottom: Int) {
         super.onLayout(changed, left, top, right, bottom)
 
-        children.filter { it.tag != "ignore" }.forEachIndexed { index, view ->
-            view.updateLayoutParams<FrameLayout.LayoutParams> {
-                gravity = Gravity.CENTER
+        children.filter { it.tag != ignoreTag }
+            .forEachIndexed { index, view ->
+                view.updateLayoutParams<LayoutParams> {
+                    gravity = Gravity.CENTER
+                }
+                place(view, index, (width.times(0.3f)).toInt())
             }
-            place(view, index, (width * 0.3f).toInt())
-        }
     }
 
-    private fun show() {
-        val cx = (width / 2) //- 20f.dp()
-        val cy = (height / 2) //+ 20f.dp()
+    fun show() {
+        createRevealAnimation(isReverse = false)
+            .apply { doOnStart { isVisible = true } }
+            .start()
+    }
 
-        // get the final radius for the clipping circle
+    fun hide() {
+        createRevealAnimation(isReverse = true)
+            .apply { doOnEnd { isVisible = false } }
+            .start()
+    }
+
+    private fun createRevealAnimation(isReverse: Boolean): Animator {
+        val cx = width / 2
+        val cy = height / 2
+
         val finalRadius = hypot(cx.toDouble(), cy.toDouble()).toFloat()
 
-        // create the animator for this view (the start radius is zero)
-        val anim = ViewAnimationUtils.createCircularReveal(this, cx.toInt(), cy.toInt(), 20f.dp(), finalRadius)
-        anim.duration = 500
-//        anim.interpolator = OvershootInterpolator()
-        visibility = View.VISIBLE
-        anim.start()
+        return ViewAnimationUtils.createCircularReveal(
+            this,
+            cx,
+            cy,
+            if (isReverse) finalRadius else 0f,
+            if (isReverse) 0f else finalRadius
+        )
     }
 
     private fun place(child: View, position: Int, radius: Int) {
         val angleDeg = 270 + (position * 45.0)
 
         val angleRad = toRadians(angleDeg)
-        val x = radius * cos(angleRad).toFloat()
-        val y = radius * sin(angleRad).toFloat()
+        val x = radius.times(cos(angleRad).toFloat())
+        val y = radius.times(sin(angleRad).toFloat())
 
-        child.rotation = position * 45f
+        child.rotation = position.times(45f)
         child.translationX = x
         child.translationY = y
     }
